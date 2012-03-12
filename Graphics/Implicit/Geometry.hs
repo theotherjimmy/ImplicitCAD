f8146449f2ea4508b9417565aec77e4c870b3668
Chris Olah <chris@colah.ca>
2012-03-12 18:35:01 -0400

    More work on tesselation of polygons.

--- Graphics/Implicit/Geometry.hs
+++ Graphics/Implicit/Geometry.hs
@@ -19,6 +19,13 @@ import Data.Maybe (isNothing, isJust)
 
 type LineSeg = (ℝ2,ℝ2)
 
+-- | sequential pairs of elements of a list
+pairs points = [(points !!! n, points !!! (n + (1::ℕ)) ) | n <- [0 .. (length points - (1::ℕ))] ]
+
+-- | A accessor that doesn't go out of bounds
+(!!!) :: Show a => [a] -> Int -> a
+l !!! n = l !! (mod n $ length l)
+
 -- | The intersection of two line segments
 --   We consider them to be open line segments,
 --   so if an end catches we don't count it
@@ -27,28 +34,37 @@ intersect (a1, a2) (b1, b2) =
 	let
 		-- transform coordinates so a is flat
 		-- first (- a1)
+		transform_part1 :: ℝ2 -> ℝ2
+		transform_part1 p = (p-a1)
+		transform_part1_inv :: ℝ2 -> ℝ2
+		transform_part1_inv = (+a1)
 		-- this makes the result of a1 be zero
 		-- (we'll do these both together, but we need the transform of a2 now)
 		(moda2x, moda2y) = a2 - a1
-		moda2slope = moda2y/moda2x
+		moda2slope = moda2y/moda2x 
 		-- Now (x,y) -> (x,y-moda2slope*x)
+		transform_part2 :: ℝ2 -> ℝ2
+		transform_part2 (x,y) = (x,y-moda2slope*x)
+		transform_part2_inv :: ℝ2 -> ℝ2
+		transform_part2_inv (x,y) = (x,y+moda2slope*x)
 		-- this makes the final result of a2 have a 0 y component.
 		-- a1' = (0,0)
 		-- a2' = (modax, 0)
 		-- For the others, we actually have to calculate...
-		transform = (\(x,y) -> (x,y-moda2slope*x)) . (\p -> p-a1)
+		transform = transform_part2 . transform_part1
 		-- Let's havave an inverse, too
-		untransform = (\p -> p-a1) . (\(x,y) -> (x,y+moda2slope*x))
+		untransform :: ℝ2 -> ℝ2
+		untransform = transform_part1_inv . transform_part2_inv
 		(b1'x, b1'y) = transform b1
 		(b2'x, b2'y) = transform b2
 		b'slope = (b2'y - b1'y)/(b2'x - b1'x)
 		-- c will be the intersect
-		c'x = b1'x + ((0::ℝ)-b1'y)/b'slope
+		c'x = b1'x - b1'y/b'slope
 		-- a convenience func for the next part
 		-- (to account for arithmetic error, err)
-		err = (1::ℝ) - (0.000005 :: ℝ)
+		err = 0.00001 :: ℝ
 		between :: ℝ -> ℝ -> ℝ -> Bool
-		between a b p = (err*a < p && p < err*b) || (err*b < p && p < err*a)
+		between a b p = (a' < p && p < b') || (b' < p && p < a') where (a',b') = (a+err*b, b+err*a)
 	in
 		if between 0 moda2x c'x && between b1'x b2'x c'x
 		then Just $ untransform (c'x, b1'y + (c'x-b1'x)*b'slope)
@@ -70,9 +86,6 @@ distFromLineSeg p@(p1,p2) (a@(a1,a2), b@(b1,b2)) = norm (closest - p)
 
 orient points = 
 	let
-		-- A accessor that doesn't go out of bounds
-		(!!!) :: Show a => [a] -> Int -> a
-		l !!! n = l !! (mod n $ length l)
 		-- Left most point. We will see how the path behaves there to determine
 		-- its handedness
 		leftMost :: Int
@@ -112,10 +125,6 @@ orient points =
 
 tesselateLoopInterior points = 
 	let
-		pairs points = [(points !!! n, points !!! (n + (1::ℕ)) ) | n <- [0 .. (length points - (1::ℕ))] ]
-		-- A accessor that doesn't go out of bounds
-		(!!!) :: Show a => [a] -> Int -> a
-		l !!! n = l !! (mod n $ length l)
 		-- Test the handedness of a triangle.
 		-- Given an oriented loop, a triangle built from consecutive points on it
 		-- can be determined to be inside or out based on its hadedness.
@@ -126,10 +135,13 @@ tesselateLoopInterior points =
 		try n prespoints@(a:b:c:others) = traceShow prespoints $ traceShow ((a,b,c), righthanded (a,b,c),filter isJust $ map (intersect (a,c)) $ pairs prespoints ) $
 			if n > (20::ℕ) + (5::ℕ)*length points + length points^2
 			then [] -- in cases of invalid input, fail gracefully
+			else if a == b
+			then try (n+ (1::ℕ)) (a:c:others)
 			else if righthanded (a,b,c) && (all isNothing $ map (intersect (a,c)) $ pairs prespoints)
 			then (b,a,c):(try (n+ (1::ℕ)) (a:c:others) )
 			else try (n+(1::ℕ)) (b:c:others ++ [a])
 		try _ _ = []
 	in
 		try 0 $ orient points 
+
 
